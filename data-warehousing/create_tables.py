12d5953ff4219ab9b98a0ac8f276db03544b7a71
andy oh <andy_oh18@yahoo.com>
2022-02-18 15:48:35 +0800

    test

--- data-warehousing/create_tables.py
+++ data-warehousing/create_tables.py
@@ -1,7 +1,7 @@
 import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
-
+# test insert
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
